1e699b3b760d10af6fcd0191d7f36004ac040bf7
saveliew <saveliew1987@gmail.com>
2018-06-08 00:37:58 +0300

    Task11 of Chapter4

---
+++ Chapter4/Task11.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Task11
+    {
+
+        static void Main(string[] args)
+        {
+
+            decimal first = 0;
+            decimal second = 1;
+            decimal count = 1;
+
+            Console.WriteLine("#" + count + " = " + first);
+            while (count < 100)
+            {
+                decimal x = first + second;
+                first = second;
+                second = x;
+                count++;
+
+
+                Console.WriteLine("#" + count + " = " + x);
+                
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+ }
+
+//Write a program that prints on the console the first 100 numbers in the 
+//Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233
